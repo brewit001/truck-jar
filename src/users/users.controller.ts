38dc4c56b5f9c834338c48566e572dc2ea5ce91e
brewit001 <brewit001@gmail.com>
2024-02-08 10:35:30 +0530

    initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll(@Query('role') role?:'ADMIN' |"INTERN" |"ENGINEER") {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
